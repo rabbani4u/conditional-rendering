c72a39f459ee52115feb6fd63edecd05e926d025
rabbani4u <rabbani.iu31@gmail.com>
2022-12-01 13:19:17 +0600

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import Login from "./Login";
 import Input from "./Input";
 
-let isLogedin = false;
-
-function renderConditionally() {
-  if (isLogedin) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+var isLogedin = false;
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {isLogedin === true ? <h1>Hello</h1> : <Login />}
+    </div>
+  );
 }
 
 export default App;
