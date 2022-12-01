348b99751f908fe8da73f7185cea8e1168fd2fc4
rabbani4u <rabbani.iu31@gmail.com>
2022-12-01 15:18:22 +0600

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,14 @@ import Input from "./Input";
 
 var isLogedin = false;
 
+let currentTime = new Date().getHours();
+
 function App() {
   return (
     <div className="container">
-      {isLogedin === true ? <h1>Hello</h1> : <Login />}
+      {/* {isLogedin === true ? <h1>Hello</h1> : <Login />} */}
+
+      {currentTime > 12 && <h1>Hi How are you</h1>}
     </div>
   );
 }
